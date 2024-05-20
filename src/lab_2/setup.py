310814541f6d4d0991176390bbf3f445c6f74e64
FERNANDO FAJARDO <fernando.fajardo@uc.cl>
2024-05-20 15:43:32 -0400

    launch

--- src/lab_2/setup.py
+++ src/lab_2/setup.py
@@ -1,4 +1,5 @@
 from setuptools import find_packages, setup
+from glob import glob
 
 package_name = 'lab_2'
 
@@ -10,6 +11,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name, glob('launch/*.xml')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
